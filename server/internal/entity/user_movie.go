fe9b756ecf2be8a028fe99b6d0dd7961541df457
Danil-114195722 <ejyou.user@gmail.com>
2025-05-14 15:10:41 +0300

    Added length validation for title filter && Updated swagger

--- server/internal/entity/user_movie.go
+++ server/internal/entity/user_movie.go
@@ -32,7 +32,7 @@ func (UserMovie) TableName() string {
 
 //easyjson:json
 type UserMoviesFilter struct {
-	Title      string   `json:"title,omitempty" query:"title" validate:"omitempty"`
+	Title      string   `json:"title,omitempty" query:"title" validate:"omitempty,max=20"`
 	RatingFrom *float64 `json:"ratingFrom,omitempty" query:"ratingFrom" validate:"omitempty,min=0"`
 	YearFrom   int      `json:"yearFrom,omitempty" query:"yearFrom" validate:"omitempty,min=1500,max=3000"`
 	YearTo     int      `json:"yearTo,omitempty" query:"yearTo" validate:"omitempty,min=1500,max=3000"`
