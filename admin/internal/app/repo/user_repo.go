b6332673006d0f610689a545e861b556ffa8d169
Danil-114195722 <ejyou.user@gmail.com>
2025-06-03 21:35:48 +0300

    Added retries and timeout for requests to REST API

--- admin/internal/app/repo/user_repo.go
+++ admin/internal/app/repo/user_repo.go
@@ -2,10 +2,18 @@ package repo
 
 import (
 	"fmt"
+	"time"
 
 	resty "github.com/go-resty/resty/v2"
 
 	"Filmer/admin/internal/app/entity"
+)
+
+const (
+	_requestTimeout = 2 * time.Second        // timeout for requests to REST API
+	_retryCount     = 3                      // amount of retries attempts in error cases
+	_retryInitTime  = 500 * time.Millisecond // time between first request and first retry
+	_retryMaxTime   = 2 * time.Second        // max time between request and retry
 )
 
 var _ UserAPIRepo = (*userAPIRepo)(nil)
@@ -24,8 +32,12 @@ func NewUserAPIRepo(host string) UserAPIRepo {
 // GetUsersActivity sends request to REST API and gets JSON-response with slice of users activity.
 func (r *userAPIRepo) GetUsersActivity() (entity.UsersActivity, error) {
 	var result entity.UsersActivity
-	// init client
-	client := resty.New()
+	// init client with retry params
+	client := resty.New().
+		SetTimeout(_requestTimeout).
+		SetRetryCount(_retryCount).
+		SetRetryWaitTime(_retryInitTime).
+		SetRetryMaxWaitTime(_retryMaxTime)
 	// do request to REST API and parse JSON-response to result
 	_, err := client.R().
 		SetResult(&result).
